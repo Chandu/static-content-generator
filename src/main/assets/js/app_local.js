348401cf658e15715912c1fb1a813effa54ad8e8
Mr. JULY <mr_july@mail.ru>
2016-04-05 16:02:38 +0200

    improved version with Thymeleaf 3.0, Bootstrap, RequireJS, application variables (app.json) and page-related variables (<template-name>.json). Better handling of incremental builds and much more. :-)

---
+++ src/main/assets/js/app_local.js
@@ -0,0 +1,16 @@
+// Place third party dependencies in the lib folder
+//
+// Configure loading modules from the lib directory,
+// except 'app' ones,
+requirejs.config({
+    "baseUrl": "assets/lib",
+    "paths": {
+      "bootstrap": "bootstrap/javascripts/bootstrap"
+    },
+    "shim": {
+        "bootstrap": ["jquery"]
+    }
+});
+
+// Load the main app module to start the app
+requirejs(["bootstrap"]);
